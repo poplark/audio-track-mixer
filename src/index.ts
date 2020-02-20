2d2923c47166bbff5ca1d4bed3c9382d8db8e168
poplark <songkang666@126.com>
2020-02-20 12:12:54 +0800

    [fix] - 修复getTracks 无返回值的问题 [feat] - addTrack 和 remvoeTrack 改为链式调用函数 [feat] - 添加 getMixedMediaStream 方法 [feat] - 添加 getTracks 静态方法

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ interface TrackCache {
 export default class AudioTrackMixer {
   private audioContext: AudioContext
   private destinationNode: MediaStreamAudioDestinationNode
-  private caches: Map<string, TrackCache> = new Map()
+  private cache: Map<string, TrackCache> = new Map()
 
   constructor() {
     this.audioContext = new AudioContextClass();
@@ -18,35 +18,41 @@ export default class AudioTrackMixer {
   }
 
   addTrack(track: MediaStreamTrack): AudioTrackMixer {
+    if (!track.kind || track.kind !== 'audio') {
+      throw new Error('not an audio track');
+    }
     const mediaStream: MediaStream = new MediaStream();
     mediaStream.addTrack(track);
     const sourceNode: MediaStreamAudioSourceNode = this.audioContext.createMediaStreamSource(mediaStream);
     const gainNode: GainNode = this.audioContext.createGain();
     sourceNode.connect(gainNode);
     gainNode.connect(this.destinationNode);
-    this.caches[track.id] = {
+    this.cache.set(track.id, {
       track,
       mediaStream,
       sourceNode,
       gainNode
-    }
+    });
     return this;
   }
 
   removeTrack(track: MediaStreamTrack): AudioTrackMixer {
-    const cache: TrackCache | undefined = this.caches[track.id];
+    if (!track.kind || track.kind !== 'audio') {
+      throw new Error('not an audio track');
+    }
+    const cache: TrackCache | undefined = this.cache.get(track.id);
 
     if (cache) {
       cache.gainNode.disconnect(this.destinationNode);
       cache.sourceNode.disconnect(cache.gainNode);
+      this.cache.delete(track.id);
     }
-    this.caches.delete(track.id);
     return this;
   }
 
   getTracks(): Array<MediaStreamTrack> {
     const tracks: Array<MediaStreamTrack> = [];
-    this.caches.forEach(function (item: TrackCache) {
+    this.cache.forEach(function (item: TrackCache) {
       tracks.push(item.track);
     });
     return tracks;
@@ -54,5 +60,13 @@ export default class AudioTrackMixer {
 
   getMixedTrack(): MediaStreamTrack {
     return this.destinationNode.stream.getAudioTracks()[0];
+  }
+
+  getMixedMediaStream(): MediaStream {
+    return this.destinationNode.stream;
+  }
+
+  static getTracks(stream: MediaStream): Array<MediaStreamTrack> {
+    return stream.getAudioTracks();
   }
 }
--- types/index.d.ts
+++ types/index.d.ts
@@ -1,10 +1,12 @@
 export default class AudioTrackMixer {
     private audioContext;
     private destinationNode;
-    private caches;
+    private cache;
     constructor();
     addTrack(track: MediaStreamTrack): AudioTrackMixer;
     removeTrack(track: MediaStreamTrack): AudioTrackMixer;
     getTracks(): Array<MediaStreamTrack>;
     getMixedTrack(): MediaStreamTrack;
+    getMixedMediaStream(): MediaStream;
+    static getTracks(stream: MediaStream): Array<MediaStreamTrack>;
 }
